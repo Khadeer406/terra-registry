d1e8068baab3a1c51f6e3f9f553e1815e618fd2b
Khadeer406 <sayyedkhadeer21@gmail.com>
2025-08-09 16:24:55 +0530

    Update main.tf
    backend removed from child module create s3

--- create-s3/main.tf
+++ create-s3/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "my-s3-new-backend-bucket-prod"
-    key    = "prod/terraform.tfstate"
-    region = "ap-south-1"
-    encrypt = true
-  }
-}
-
 resource "aws_s3_bucket" "backend_s3" {
   bucket = var.bucket_name
 }
@@ -27,4 +18,5 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "enable_sse" {
     }
   }
   
+
 }
